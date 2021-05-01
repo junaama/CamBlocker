a4ac823949b629a4cbbd24bdde5aaff6b507f6b3
Naama <0naama0@gmail.com>
2021-05-01 15:04:11 -0400

    add options panel

--- content.js
+++ content.js
@@ -1,1 +1,20 @@
-alert("Hello from A Coder")
+alert("Hello from CamBlocker")
+
+const init = () => {
+    if (navigator.webkitGetUserMedia) {
+      navigator.webkitGetUserMedia(
+        { video: true, audio: true },
+        onSuccess,
+        onFail
+      );
+    } else {
+      alert("Camera is not available");
+    }
+  };
+  
+  const onSuccess = () => {};
+  
+  const onFail = () => {
+    alert("Could not connect to stream. Please allow camera permission!");
+  };
+init();
